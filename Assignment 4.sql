1839f1c0655206a7b988821b20d8ed2a564171f0
kaushikharshita15 <51269112+kaushikharshita15@users.noreply.github.com>
2022-08-29 18:39:06 +0530

    Update Assignment 4.sql

--- Assignment 4.sql
+++ Assignment 4.sql
@@ -13,3 +13,7 @@ values
 ('1','My First SQL Book','Mary Poker','981483029127','2012-02-22 12:08:17'),
 ('2','My Second SQL Book','John Mayer','857300923713','1972-07-03 09:22:45'),
 ('3','My Third SQL Book','Cary Flint','523120967812','2015-10-18 14:05:44');
+
+SELECT * FROM books
+WHERE author LIKE '%er';
+
