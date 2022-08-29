0d02e6a48dae1f931006fb2341b8c0b6f5d758f9
kaushikharshita15 <51269112+kaushikharshita15@users.noreply.github.com>
2022-08-29 18:37:12 +0530

    Update Assignment 4.sql

--- Assignment 4.sql
+++ Assignment 4.sql
@@ -1,8 +1,15 @@
-    CREATE TABLE books
+ -- Scenario 1
+   CREATE TABLE books
 (
     id int primary key,
     title varchar(20),
     author varchar(20),
-	isbn int,
-	published_date date 
+	isbn bigint,
+	published_date datetime; 
 )
+
+insert into books(id,title,author,isbn,published_date)
+values 
+('1','My First SQL Book','Mary Poker','981483029127','2012-02-22 12:08:17'),
+('2','My Second SQL Book','John Mayer','857300923713','1972-07-03 09:22:45'),
+('3','My Third SQL Book','Cary Flint','523120967812','2015-10-18 14:05:44');
